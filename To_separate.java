40a00e8c9ea3c195dadc519515d512669e1c4862
Vaghela Madhuri <madhurivaghela463@gmail.com>
2026-09-19 12:17:07 +0530

    To_separate.java
    n + n = 2n
    Constants ignore કરીએ → O(n)

---
+++ To_separate.java
@@ -0,0 +1,12 @@
+public class Main {
+	public static void main(String[] args) {
+		int n = 10;
+		for (int i = 0; i < n; i++) {
+			System.out.println(i);
+		}
+
+		for (int j = 0; j < n; j++) {
+			System.out.println(j);
+		}
+	}
+}
