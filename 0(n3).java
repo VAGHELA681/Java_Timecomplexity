a4e6b6472fd681247cad6b913aa5c5749872b377
Vaghela Madhuri <madhurivaghela463@gmail.com>
2026-09-19 13:48:03 +0530

    0(n3).java
    n * n * n = n3
    O(n3)

---
+++ 0(n3).java
@@ -0,0 +1,12 @@
+public class Main {
+	public static void main(String[] args) {
+		int n =1;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				for (int k = 0; k < n; k++) {
+					System.out.println(i + j + k);
+				}
+			}
+		}
+	}
+}
