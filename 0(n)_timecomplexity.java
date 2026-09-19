9d6bc6997d01ebb6f73e872bc2ad3ea3222ce53a
Vaghela Madhuri <madhurivaghela463@gmail.com>
2026-09-19 12:37:27 +0530

    Add 0(n) time complexity example in Java0(n)_timecomplexity.java
    n * 5 = 5n
    = 0(n) time complexity

---
+++ 0(n)_timecomplexity.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String[] args) {
+		int n = 5;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < 5; j++) {
+				System.out.println(i + j);
+			}
+		}
+	}
+}
